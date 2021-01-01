b7d3ae5527820d38906a8a3bb19978bcc44f02df
JoeParrinello <joe@parrinello.me>
2021-01-01 17:40:46 -0500

    Hello World! Basic Templating and Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# Start from a Debian image with the latest version of Go installed
+# and a workspace (GOPATH) configured at /go.
+FROM golang
+
+# Copy the local package files to the container's workspace
+ADD . /go/src/hardorange/fitness
+
+# Build the fitness command inside the container.
+RUN go get /go/src/hardorange/fitness
+RUN go install /go/src/hardorange/fitness
+
+# Run the fitness
+ENTRYPOINT /go/bin/fitness
---
+++ fitness.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+	"time"
+)
+
+type exercise struct {
+	Name string
+	Reps int
+}
+
+type pageData struct {
+	DateString string
+	WeekNum    int
+	DayNum     int
+	Exercises  []exercise
+}
+
+func main() {
+	http.HandleFunc("/", handleHome)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
+	}
+
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal("server encountered error", err)
+	}
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	log.Println("serving home")
+	tmpl := template.Must(template.ParseFiles("home.html"))
+	tmpl.Execute(w, getPageData())
+}
+
+func getPageData() pageData {
+	data := pageData{
+		DateString: "Jan 1",
+		WeekNum:    getWeekNum(),
+		DayNum:     getDayNum(),
+		Exercises: []exercise{
+			getPushUpExercise(),
+			getSitUpExercise(),
+			getJumpingJackExercise(),
+		},
+	}
+	return data
+}
+
+func getPushUpExercise() exercise {
+	return exercise{
+		Name: "Push Ups",
+		Reps: getWeekNum(),
+	}
+}
+
+func getSitUpExercise() exercise {
+	return exercise{
+		Name: "Sit Ups",
+		Reps: getWeekNum(),
+	}
+}
+
+func getJumpingJackExercise() exercise {
+	return exercise{
+		Name: "Jumping Jacks",
+		Reps: getDayNum(),
+	}
+}
+
+func getWeekNum() int {
+	return (getDayNum() / 7) + 1
+}
+
+func getDayNum() int {
+	return time.Now().YearDay()
+}
