6e31857137c8aabda6f4dfe2f33e7df205047bce
JoeParrinello <joe@parrinello.me>
2021-01-10 16:19:48 -0500

    Formt doc, and cache the home page

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,48 @@
 var cacheName = 'fitnessPWA-v2';
 var appShellFiles = [
-    '/static/assets/dumbbell_32.png',
-    '/static/assets/dumbbell_128.png',
-    '/static/assets/dumbbell_256.png',
-    '/static/assets/favicon.ico',
-    '/static/scripts/app.js',
-    '/static/stylesheets/home.css',
+  '/static/assets/dumbbell_32.png',
+  '/static/assets/dumbbell_128.png',
+  '/static/assets/dumbbell_256.png',
+  '/static/assets/favicon.ico',
+  '/static/scripts/app.js',
+  '/static/stylesheets/home.css',
+  '/',
 
 ];
 
 self.addEventListener('install', (e) => {
-    console.log('[ServiceWorker] Install');
-    e.waitUntil(
-        caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching all: app shell and content');
-            return cache.addAll(appShellFiles);
-        })
-    );
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(appShellFiles);
+    })
+  );
 });
 
 self.addEventListener('activate', (e) => {
-    e.waitUntil(
-        caches.keys().then((keyList) => {
-            return Promise.all(keyList.map((key) => {
-                if (key !== cacheName) {
-                    return caches.delete(key);
-                }
-            }));
-        })
-    );
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((r) => {
-            console.log('[Service Worker] Fetching resource: ' + e.request.url);
-            return r || fetch(e.request).then((response) => {
-                return caches.open(cacheName).then((cache) => {
-                    console.log('[Service Worker] Caching new resource: ' + e.request.url);
-                    cache.put(e.request, response.clone());
-                    return response;
-                });
-            });
-        })
-    );
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+      console.log('[Service Worker] Fetching resource: ' + e.request.url);
+      return r || fetch(e.request).then((response) => {
+        return caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching new resource: ' + e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
 });
